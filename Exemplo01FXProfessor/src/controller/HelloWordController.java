0e2bedcd48fb5b102e1db2c1f7a3623c51e16128
andre.forchesatto <andreforchesatto@gmail.com>
2016-03-22 20:32:53 -0300

    Criado controller HelloWord

---
+++ Exemplo01FXProfessor/src/controller/HelloWordController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HelloWordController {
+
+	@FXML
+	private TextField tfNome;
+
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnExcluir;
+
+	@FXML
+	void excluir(ActionEvent event) {
+
+	}
+
+	@FXML
+	void salvar(ActionEvent event) {
+
+	}
+
+}
