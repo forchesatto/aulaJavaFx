47d4d6fb8268a59bb8975a872f19f4b75990d735
andre.forchesatto <andreforchesatto@gmail.com>
2016-03-22 23:53:40 -0300

    Ajustes no alert de excluir e no conteúdo da tabela para mostrar o nome

--- Exemplo01FXProfessor/src/controller/HelloWordController.java
+++ Exemplo01FXProfessor/src/controller/HelloWordController.java
@@ -1,9 +1,17 @@
 package controller;
 
+import java.util.Optional;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class HelloWordController {
@@ -21,8 +29,41 @@ public class HelloWordController {
 	private Label lblMsgSalvar;
 
 	@FXML
-	void excluir(ActionEvent event) {
+	private TableView<String> tblNomes;
 
+	@FXML
+	private TableColumn<String, String> colNome;
+
+	/**
+	 * Será executado logo após o arquivo xml ser carregado é a primeira coisa a
+	 * ser executado depois de carregar o xml.
+	 */
+	@FXML
+	private void initialize() {
+		// Seta o responsavel por fabricar os valores da coluna.
+		colNome.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue()));
+	}
+
+	@FXML
+	void excluir(ActionEvent event) {
+		Alert alerta = new Alert(AlertType.CONFIRMATION, "Deseja realmente excluir?", ButtonType.CANCEL, ButtonType.OK);
+
+		// Desativando o comportamento padrão.
+		Button okButton = (Button) alerta.getDialogPane().lookupButton(ButtonType.OK);
+		okButton.setDefaultButton(false);
+
+		// Optional do Java 8 executa o show e fica aguardando o click do botão.
+		final Optional<ButtonType> result = alerta.showAndWait();
+		// Se o click foi no ok executa os comandos abaixo
+		if (result.get() == ButtonType.OK) {
+			// Pega a linha selecionada
+			int posicaoTabela = tblNomes.getSelectionModel().getSelectedIndex();
+			// remove da tabela a linha selecionada
+			tblNomes.getItems().remove(posicaoTabela);
+			lblMsgSalvar.setText("Registro Excluido com sucesso");
+			lblMsgSalvar.getStyleClass().remove("msgSalvar");
+			lblMsgSalvar.getStyleClass().add("msgExcluir");
+		}
 	}
 
 	@FXML
@@ -32,7 +73,10 @@ public class HelloWordController {
 		String msg = "Salvo com sucesso o cadastro: ";
 		// Seta no label a mensagem de salvo com sucesso
 		lblMsgSalvar.setText(msg + nome);
+		lblMsgSalvar.getStyleClass().remove("msgExcluir");
 		lblMsgSalvar.getStyleClass().add("msgSalvar");
+		// Seta cada um dos nomes para dentro da tabela
+		tblNomes.getItems().add(nome);
 	}
 
 }
